d8241e7e42436755f29d3e46c897606ca393335c
MARTYNAS-HI <78751357+MARTYNAS-HI@users.noreply.github.com>
2022-02-11 16:37:37 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+song1 = "";
+song2 = "";
+song1_status = "";
+song2_status = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+function preload()
+{
+    song1 = loadSound("music.mp3");
+    song2 = loadSound("A Morning in Hindight.mp3");
+}
+
+
+function setup() {
+   
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+    
+    video = createCapture(VIDEO);
+    video.hide();
+    
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet is Initialized');
+}
+
+function draw(); {
+image(video, 0, 0, 600, 500);
+}
+
+function play()
+{
+    song.play
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+    console.log(results);
+    leftWristX = results[0].pose.leftWrist.x;
+    leftWristY = results[0].pose.leftWrist.y;
+    console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
+        
+    rightWristX = results[0].pose.rightWrist.x;
+    rightWristY = results[0].pose.rightWrist.y;
+    console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);   
+    }
+}
+
+
+
+
+
+
+
+
